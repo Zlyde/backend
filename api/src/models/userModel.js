c7569d57e6a5c139f2198f21e072d600c7140e02
Hanna Gawlitza <hagw23@student.bth.se>
2024-12-22 09:21:22 +0100

    userModel

--- api/src/models/userModel.js
+++ api/src/models/userModel.js
@@ -15,8 +15,8 @@ const userSchema = new mongoose.Schema({
     },
     email: { 
         type: String,
-        required: [true, 'Email is required'], 
         unique: true,
+        sparse: true,
         match: [/\S+@\S+\.\S+/, 'Email format is invalid'],
         index: true 
     },
